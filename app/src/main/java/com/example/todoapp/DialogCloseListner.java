c1451e105de41865e9499fda0e048ae7de2ad7b2
Harman <harmanpreet181001@gmail.com>
2022-06-18 22:05:52 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/todoapp/DialogCloseListner.java
@@ -0,0 +1,10 @@
+package com.example.todoapp;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+
+public interface DialogCloseListner {
+
+    public void handleDialogClose(DialogInterface dialog);
+
+}
